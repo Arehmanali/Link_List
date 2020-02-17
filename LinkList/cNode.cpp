62a3574285ac4b795417edf87b4436488f3238e1
Rehman Ali <55204040+Arehmanali@users.noreply.github.com>
2020-02-17 22:44:16 +0500

    Update cNode.cpp

--- LinkList/cNode.cpp
+++ LinkList/cNode.cpp
@@ -5,7 +5,7 @@ void cNode:: setValue(int v)
 	value = v;
 }
 
-// getter function for getting value of node
+/* getter function for getting value of node */
 int cNode:: getValue()
 {
 	return value;
